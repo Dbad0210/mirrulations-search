1873c6e172968621784f6080718f02e811f275ca
Jeffery Eisenhardt <eisenhardtj@moravian.edu>
2026-02-13 14:18:30 -0500

    Changed the up script to setup the environment and fully run on port 80

--- up.sh
+++ up.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+python3 -m venv .venv
+
+source .venv/bin/activate
+
+pip install -r requirements.txt
+
+pip install -e .
+
 # Sets the PWD to the src directory and starts the gunicorn server on port 80 using the configuration in conf/gunicorn.py.
 export PYTHONPATH="$PWD/src"
-exec gunicorn -c conf/gunicorn.py mirrsearch.app:app --bind 0.0.0.0:80
-
+sudo .venv/bin/gunicorn -c conf/gunicorn.py mirrsearch.app:app --bind 0.0.0.0:80
