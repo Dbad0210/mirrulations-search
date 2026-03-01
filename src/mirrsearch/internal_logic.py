acc7fd8633454986234b00492442adecab593a74
Vicente R <riverav@moravian.edu>
2026-03-01 13:26:36 -0500

    Update to internal_logic.py for mock frontend testing

--- src/mirrsearch/internal_logic.py
+++ src/mirrsearch/internal_logic.py
@@ -9,4 +9,16 @@ class InternalLogic:  # pylint: disable=too-few-public-methods
     def search(self, query, document_type_param=None, agency=None, cfr_part_param=None):
         db_layer = self.db_layer if self.db_layer is not None else get_db()
         search_results = db_layer.search(query, document_type_param, agency, cfr_part_param)
+
+        search_results = [
+            {
+                "docket_id": "Test_Id",
+                "title": "Test Title",
+                "cfrPart": "Test CFR Part",
+                "agency_id": "Test Agency ID",
+                "docket_type": "Test Docket Type",
+                "modified_date": "2026-03-01"
+
+            }
+        ]
         return search_results
